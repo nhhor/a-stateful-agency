f60e17151fe72267ad7c7155f1bdc5f4b88c5744
Student <example@example.com>
2020-02-17 10:54:27 -0800

    initilize project from template.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import Header from './Header';
+import Footer from './Footer';
+import Home from './Home';
+import AboutUs from './AboutUs';
+
+import { Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Header/>
+
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/aboutus' component={AboutUs} />
+      </Switch>
+
+      <div className="footer">
+
+        <Footer/>
+      </div>
+
+      <style jsx>{`
+      .App {
+        color: green;
+      }
+
+      .footer{
+      position: fixed;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      background-color: red;
+      color: white;
+      text-align: center;
+    }
+      `}</style>
+
     </div>
   );
 }
